4ede6eb8bdcbaad2cc0387c57dcad688f7cc7056
pautips03 <166941828+pautips03@users.noreply.github.com>
2024-04-13 12:03:17 -0500

    Add files via upload

---
+++ const openPopupBtn= document.js
@@ -0,0 +1,6 @@
+const openPopupBtn= document.getElementById('openPopupBtn');
+const popup = document.getElementById ('popup');
+
+openPopupBtn.addEventListener('click',function(){
+    popup.style.display = 'block';
+});
